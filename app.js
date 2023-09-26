41b4369635c3410546a342d4b098135e95127ee7
mehmetaln <mehmettalnn@gmail.com>
2023-09-26 11:48:46 +0300

    devam

--- app.js
+++ app.js
@@ -180,4 +180,20 @@ link.className="btn btn-dark btn-sm mt-3";
 link.href = "https://github.com/mehmetaln";
 link.target = "_blank";
 link.innerHTML = "Benim Web Siteme Git";
+cardBody.appendChild(link);
 console.log(link);
+
+
+const todo = document.createElement("li");
+const todoLink = document.createElement("a");
+const i = document.createElement("i");
+todo.className= "list-group-item d-flex justify-content-between";
+todo.innerHTML ="Todo 5"
+
+todoLink.href="#";
+todoLink.className = "delete-item"
+
+i.className= "fa fa-remove";
+
+todoLink.appendChild(i);
+todo.appendChild(todoLink);
